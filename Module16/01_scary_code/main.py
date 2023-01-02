321ac577a514810979a68ca90000073e943a32b8
Vladislav Bulanov <solist95@mail.ru>
2023-01-02 19:02:04 +0300

    tasks 1-4 are done

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,20 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+# Возможно, здесь решать через функцию излишне,
+# просто захотелось попрактиковаться :)
+def add_count_delete(source_list, additional_list, digit, merge_number, is_delete):
+    source_list.extend(additional_list)
+    count_digit = source_list.count(digit)
+    print(f'Количество цифр {digit} при {merge_number}-ом объединении: {count_digit}')
 
-# Initial commit.
+    if is_delete:
+        for _ in range(count_digit):
+            source_list.remove(digit)
+
+
+prime_list = [1, 5, 3]
+side_list_1 = [1, 5, 1, 5]
+side_list_2 = [1, 3, 1, 5, 3, 3]
+
+add_count_delete(prime_list, side_list_1, 5, 1, True)
+add_count_delete(prime_list, side_list_2, 3, 2, False)
+
+print('Итоговый список:', prime_list)
--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,18 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+item = input('Введите название детали: ').lower()
+total_item_count = 0
+total_price = 0
+
+for element in shop:
+    item_count = element.count(item)
+    if item_count:
+        total_price += element[1]
+        total_item_count += item_count
+
+if total_item_count:
+    print('Количество деталей:', total_item_count)
+    print('Общая стоимость:', total_price)
+else:
+    print('Такой детали в магазине нет.')
