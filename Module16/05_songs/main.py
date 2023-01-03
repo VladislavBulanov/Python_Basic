aeb5d12d43f17227ebcde23f520534d175174e82
Vladislav Bulanov <solist95@mail.ru>
2023-01-03 16:12:10 +0300

    tasks 5-7 are done

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -9,5 +9,14 @@ violator_songs = [
     ['Blue Dress', 4.29],
     ['Clean', 5.83]
 ]
+total_time = 0
+quantity = int(input('Введите количество песен: '))
 
-# TODO здесь писать код
+for index in range(quantity):
+    song = input(f'Название {index + 1}-ой песни: ')
+
+    for element in violator_songs:
+        if song in element:
+            total_time += element[1]
+
+print(f'\nОбщее время звучания песен: {round(total_time, 2)} минут(ы).')
--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+def create_list(elements_quantity, list_number):
+    result_list = []
+
+    for index in range(elements_quantity):
+        input_number = int(input(f'Введите {index + 1}-е число для {list_number}-го списка: '))
+        result_list.append(input_number)
+
+    return result_list
+
+
+first_list = create_list(3, 1)
+second_list = create_list(7, 2)
+print('\nПервый список:', first_list)
+print('Второй список:', second_list)
+
+first_list.extend(second_list)
+
+# Идём по объединённому списку, берём каждое число, проверяем, сколько
+# раз оно встречается в списке, и удаляем N-1 раз:
+for number in first_list:
+    total_number_appears = first_list.count(number)
+    for time in range(total_number_appears - 1):
+        first_list.remove(number)
+
+print('\nНовый первый список с уникальными элементами:', first_list)
