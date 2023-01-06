27618b0e32da60031ba59f78d2a3783cd3741331
Vladislav Bulanov <solist95@mail.ru>
2023-01-06 19:32:13 +0300

    Divided the function into three ones: extend_list, count_digit and delete_digit

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,20 +1,25 @@
-# Возможно, здесь решать через функцию излишне,
-# просто захотелось попрактиковаться :)
-def add_count_delete(source_list, additional_list, digit, merge_number, is_delete):
+def extend_list(source_list, additional_list):
     source_list.extend(additional_list)
-    count_digit = source_list.count(digit)
-    print(f'Количество цифр {digit} при {merge_number}-ом объединении: {count_digit}')
 
-    if is_delete:
-        for _ in range(count_digit):
-            source_list.remove(digit)
+
+def count_digit(initial_list, number):
+    return initial_list.count(number)
+
+
+def delete_digit(source_list, digit):
+    for _ in range(source_list.count(digit)):
+        source_list.remove(digit)
 
 
 prime_list = [1, 5, 3]
 side_list_1 = [1, 5, 1, 5]
 side_list_2 = [1, 3, 1, 5, 3, 3]
 
-add_count_delete(prime_list, side_list_1, 5, 1, True)
-add_count_delete(prime_list, side_list_2, 3, 2, False)
+extend_list(prime_list, side_list_1)
+print(f'Количество цифр 5 при первом объединении: {count_digit(prime_list, 5)}')
+delete_digit(prime_list, 5)
+
+extend_list(prime_list, side_list_2)
+print(f'Количество цифр 3 при втором объединении: {count_digit(prime_list, 3)}')
 
 print('Итоговый список:', prime_list)
