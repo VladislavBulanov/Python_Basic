6efa2de608e4b34e734b10ac6de2dcef4cf35b62
Vladislav Bulanov <solist95@mail.ru>
2023-01-07 10:55:09 +0300

    Added the function count_of_item

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -1,16 +1,23 @@
+def count_of_item(source_item, source_items_list):
+    result_list = [0, 0]  # [0] - total count, [1] - total price
+    for element in source_items_list:
+        item_quantity = element.count(source_item)
+        if item_quantity:
+            result_list[0] += 1
+            result_list[1] += element[1]
+
+    return result_list
+
+
 shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
 item = input('Введите название детали: ').lower()
-total_item_count = 0
-total_price = 0
 
-for element in shop:
-    item_count = element.count(item)
-    if item_count:
-        total_price += element[1]
-        total_item_count += item_count
+data_list = count_of_item(item, shop)
+total_item_count = data_list[0]
+total_price = data_list[1]
 
 if total_item_count:
     print('Количество деталей:', total_item_count)
