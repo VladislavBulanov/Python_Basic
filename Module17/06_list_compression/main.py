1112de622b022263761781d6550e0f8287cc974b
Vladislav <vladislav.bulanov1895@gmail.com>
2023-01-12 16:20:15 +0300

    Tasks n.4-6 are done

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import random
+
+def fill_the_numbers_list(quantity):
+    return [random.randint(0, 2) for _ in range(quantity)]
+
+
+def delete_all_nulls(source_list):
+    result_list = [number for number in source_list]
+
+    while result_list.count(0):
+        result_list.remove(0)
+
+    return result_list
+
+
+numbers_quantity = int(input('Введите количество чисел в списке: '))
+numbers_list = fill_the_numbers_list(numbers_quantity)
+print('Список до сжатия:', numbers_list)
+print('Список после сжатия:', delete_all_nulls(numbers_list))
--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+my_list = [[j_num for j_num in range(i_list, 13, 4)] for i_list in range(1, 5)]
+print(my_list)
+second_answer = [[value, value + 4, value + 8] for value in range(1, 5)]
+print(second_answer)
