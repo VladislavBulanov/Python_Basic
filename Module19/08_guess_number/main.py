7b06d6bce4c9497193e6aefeccefffc755b8d311
Vladislav <vladislav.bulanov1895@gmail.com>
2023-01-25 00:05:03 +0300

    Task n.8 are finished

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -2,20 +2,21 @@ import random
 
 limit_number = int(input('Введите максимальное число: '))
 guess_number = random.randint(1, limit_number)
-right_numbers = set()
+potential_right_numbers = set()
 wrong_numbers = set()
 
-while (guess_numbers := input('Нужное число есть среди этих чисел: ')) != 'Помогите!':
-    numbers = {int(number) for number in guess_numbers.split()}
+while (inputted_guess_numbers := input('\nНужное число '
+       'есть среди этих чисел: ')) != 'Помогите!':
 
+    numbers = {int(number) for number in inputted_guess_numbers.split()}
     for current_number in numbers:
         if current_number == guess_number:
             print('Ответ Артёма: Да')
-            right_numbers.update(numbers - wrong_numbers)
+            potential_right_numbers.update(numbers - wrong_numbers)
             break
     else:
         print('Ответ Артёма: Нет')
-        right_numbers.difference_update(numbers)
+        potential_right_numbers.difference_update(numbers)
         wrong_numbers.update(numbers)
 
-print('Артём мог загадать следующие числа:', right_numbers)
+print('\nАртём мог загадать следующие числа:', *potential_right_numbers)
