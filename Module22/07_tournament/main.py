3006aa4f70a05f7fb4d5819a2580a85637b2bd65
Vladislav Bulanov <solist95@mail.ru>
2023-02-17 22:15:50 +0300

    tasks n.1-7 are done

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,1 +1,42 @@
-# TODO здесь писать код
+import os
+
+def get_next_tour_participants(datafile_path):
+    datafile = open(datafile_path, 'r')
+    data = datafile.read().split('\n')
+
+    points_to_pass = int(data[0])
+    next_tour_participants = dict()
+
+    for index in range(1, len(data)):
+        current_participant = data[index].split()
+        if int(current_participant[2]) > points_to_pass:
+            next_tour_participants[f'{current_participant[1][:1]}. '
+                                   f'{current_participant[0]}'] = \
+                                   int(current_participant[2])
+
+    datafile.close()
+    return sort_dictionary_by_value(next_tour_participants)
+
+
+def write_information_to_file(required_path, statistics):
+    result_file = open(required_path, 'a')
+
+    result_file.write(f'{str(len(statistics))}\n')
+    position = 1
+    for player, points in statistics.items():
+        result_file.write(f'{position}) {player} {points}\n')
+        position += 1
+
+    result_file.close()
+
+
+def sort_dictionary_by_value(dictionary):
+    inverted_dictionary = {value: key for key, value in dictionary.items()}
+    sorted_keys_list = sorted(list(inverted_dictionary.keys()), reverse=True)
+    return {inverted_dictionary[key]: key for key in sorted_keys_list}
+
+
+path_to_datafile = os.path.abspath('first_tour.txt')
+path_to_result_file = os.path.abspath('second_tour.txt')
+next_tour_information = get_next_tour_participants(path_to_datafile)
+write_information_to_file(path_to_result_file, next_tour_information)
--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+
+def get_texts_statistics(datafile_path):
+    source_file = open(datafile_path, 'r')
+    source_text = source_file.read()
+
+    letters_in_text = sorted([
+        symbol.lower()
+        for symbol in source_text
+        if symbol.isalpha()
+    ])
+
+
+
+
+path_to_datafile = os.path.abspath('text.txt')
+texts_statistics = get_texts_statistics(path_to_datafile)
