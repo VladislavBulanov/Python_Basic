0acba15d721fa4d3f09261da317abad227d873b7
Vladislav Bulanov <solist95@mail.ru>
2023-02-22 16:59:11 +0300

    Task n.5 is done

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -13,22 +13,44 @@ def run_text_calculator(data_file_path):
                                                       current_line[1],
                                                       current_line[2])
                 except (SyntaxError, ValueError, ZeroDivisionError) as error:
+                    print(f'\nОбнаружена следующая ошибка в строке <{" ".join(current_line)}>:')
                     print(error)
+                    changed_line = change_line()
+                    if changed_line:
+                        total_sum += calculate_expression(changed_line[0],
+                                                          changed_line[1],
+                                                          changed_line[2])
+
+        print('\nСумма результатов:', total_sum)
 
     except FileNotFoundError:
         print('Неверно указан путь к исходному файлу.')
-    finally:
-        print('\nСумма результатов:', total_sum)
 
 
-def validate_data(data_list):
+def validate_data(data_list: list):
+    """
+    :param data_list: expected list: [number_1, operation, number_2]
+    :return: None, if errors have not been raised
+    :raises SyntaxError, ValueError, ZeroDivisionError
+    """
     if len(data_list) != 3:
-        raise SyntaxError('Не соблюдена схема "ОПЕРАНД_1 ОПЕРАЦИЯ ОПЕРАНД_2".')
+        raise SyntaxError('\tне соблюдена схема <ОПЕРАНД_1 ОПЕРАЦИЯ ОПЕРАНД_2>')
+    if data_list[1] not in ('+', '-', '*', '/', '//', '%', '**'):
+        raise SyntaxError(f'\tнекорректная операция: {data_list[1]}')
     if (not data_list[0].isdigit()) or (not data_list[2].isdigit()):
-        raise ValueError('Операнд не является целым числом.')
+        raise ValueError('\tоперанд не является целым числом')
+    if (data_list[1] in ('/', '//', '%')) and (int(data_list[2]) == 0):
+        raise ZeroDivisionError('\tделение на ноль запрещено')
 
 
 def calculate_expression(number_1, operation, number_2):
+    """
+    :type number_1: str
+    :type operation: str
+    :type number_2: str
+    :rtype int or float
+    :raise: ZeroDivisionError
+    """
     if operation == '+':
         return int(number_1) + int(number_2)
     elif operation == '-':
@@ -43,9 +65,28 @@ def calculate_expression(number_1, operation, number_2):
         return int(number_1) % int(number_2)
     elif operation == '**':
         return int(number_1) ** int(number_2)
-    else:
-        raise ValueError(f'Некорректная операция: {operation}.')
-# TODO Убрать эту ошибку в функцию валидности
+
+
+def change_line():
+    """
+    :return: None, if user doesn't want to change wrong line,
+             and correct line, if he does.
+    :rtype: list
+    """
+    user_choice = input('Хотите исправить ошибку? (Y/N): ').lower()
+    if user_choice == 'n':
+        return None
+    elif user_choice == 'y':
+        while True:
+            right_line = input('\nВведите исправленную строку: ').strip().split()
+            try:
+                validate_data(right_line)
+            except (SyntaxError, ValueError, ZeroDivisionError) as error:
+                print(f'Некорректный ввод: {error}')
+                print('Пожалуйста, повторите ввод.')
+            else:
+                return right_line
+
 
 path_to_data_file = os.path.abspath('calc.txt')
 run_text_calculator(path_to_data_file)
