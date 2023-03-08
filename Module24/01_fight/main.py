f81219d600be3d90c36b1b820ffbd024f2ed054e
Vladislav <vladislav.bulanov1895@gmail.com>
2023-03-08 21:30:27 +0300

    Fix: added the checking of unit's class

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,24 +1,30 @@
 from random import randint
 
+
 class Warrior:
-    health = 100
+
+    def __init__(self, health=100):
+        self.health = health
 
     def loose_health(self, damage):
-        self.health -= damage
+        if isinstance(self, Warrior):
+            self.health -= damage
+        else:
+            print('Атакуемый объект не является воином.')
 
 
 def start_fight(unit_1, unit_2):
     print('ДРАКА НАЧАЛАСЬ!')
     while unit_1.health > 0 and unit_2.health > 0:
         who_beats = randint(1, 2)
-        print(f'\nБьёт воин №{who_beats}:')
+        print('\nБьёт воин №{}:'.format(who_beats))
 
         if who_beats == 1:
             unit_2.loose_health(20)
-            print(f'У воина №2 осталось {unit_2.health} очков здоровья.')
+            print('У воина №2 осталось {} очков здоровья.'.format(unit_2.health))
         elif who_beats == 2:
             unit_1.loose_health(20)
-            print(f'У воина №1 осталось {unit_1.health} очков здоровья.')
+            print('У воина №1 осталось {} очков здоровья.'.format(unit_1.health))
 
     if unit_1.health <= 0:
         print('\nПОБЕДУ ОДЕРЖАЛ ВОИН №2!')
