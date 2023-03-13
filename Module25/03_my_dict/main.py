eebb5929dbbd9629ce5a113147faf6f184c27f10
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-03-13 18:09:02 +0300

    Backup: 2023-03-13_nb_linux

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+class MyDict:
+    def __init__(self, dictionary: dict):
+        self.__dictionary = dictionary
+
+    def get_value(self, key):
+        if key in self.__dictionary:
+            return self.__dictionary[key]
+        else:
+            return 0
+
+    def set_key_value(self, key, value):
+        self.__dictionary[key] = value
+
+    def show_dictionary(self):
+        for key, value in self.__dictionary.items():
+            print('{}: {}'.format(key, value))
+
+
+my_dict = MyDict(dict())
+my_dict.set_key_value('t', 5)
+my_dict.set_key_value('t', 6)
+my_dict.set_key_value(8, 'r')
+print(my_dict.get_value(8))
+my_dict.show_dictionary()
