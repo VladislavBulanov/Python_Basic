916da6c4c0412556f5d3bdacd2429ba5a89b638d
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-03-26 15:39:51 +0300

    Backup: 2023-03-26_nb_ubuntu

--- Module25/06_cohabitation_2/classes.py
+++ Module25/06_cohabitation_2/classes.py
@@ -56,7 +56,10 @@ class House:
 
 
 class Human:
-    """The class describing human."""
+    """
+    The class describing human. Each action (except eat) decreases
+    degree of satiety by 10 points.
+    """
     def __init__(self, name: str, satiety: int = 30, happy: int = 100):
         """
         Class constructor.
@@ -125,10 +128,95 @@ class Human:
         human's degree of happy by 5 points.
         """
         self.set_happy(self.get_happy() + 5)
+        self.set_satiety(self.get_satiety() - 10)
+
+
+class Husband(Human):
+    """
+    The subclass of class 'Human' describing husband. Each action
+    (except eat) decreases degree of satiety by 10 points.
+    """
+    def __init__(self, name: str):
+        """Class constructor."""
+        super().__init__(name)
+
+    def game(self):
+        """Husband plays computer. It increases his happy by 20 points."""
+        self.set_happy(self.get_happy() + 20)
+        self.set_satiety(self.get_satiety() - 10)
+
+    def go_work(self, house: House):
+        """
+        Husband goes to the work and brings 150 units of money in house.
+        :param house: house where husband lives
+        """
+        house.set_money(house.get_money() + 150)
+        self.set_satiety(self.get_satiety() - 10)
+
+
+class Wife(Human):
+    """
+    The subclass of class 'Human' describing wife. Each action
+    (except eat) decreases degree of satiety by 10 points.
+    """
+    def __init__(self, name: str):
+        """Class constructor."""
+        super().__init__(name)
+
+    def buy_humans_food(self, house: House):
+        """
+        Wife buys human's food. It costs 10 units of money
+        and increases human's food in the house by 10 units.
+        :param house: house where wife lives
+        """
+        house.set_money(house.get_money() - 10)
+        house.set_humans_food(house.get_humans_food() + 10)
+        self.set_satiety(self.get_satiety() - 10)
+
+    def buy_cats_food(self, house: House):
+        """
+        Wife buys cat's food. It costs 10 units of money
+        and increases cat's food in the house by 10 units.
+        :param house: house where wife lives
+        """
+        house.set_money(house.get_money() - 10)
+        house.set_cats_food(house.get_cats_food() + 10)
+        self.set_satiety(self.get_satiety() - 10)
+
+    def buy_fur_coat(self, house: House, money_limit: int = 400):
+        """
+        Wife buys a fur coat. It costs 350 units of money and gives 60 points
+        of happy. Wife can make purchase if there is enough quantity of money
+        (400 units by default).
+        :param house: the house where wife lives
+        :param money_limit: the quantity of money in the house allows
+        to make a purchase
+        """
+        if house.get_money() >= money_limit:
+            house.set_money(house.get_money() - 350)
+            self.set_happy(self.get_happy() + 60)
+            self.set_satiety(self.get_satiety() - 10)
+        else:
+            print('Сейчас недостаточно денег для покупки шубы.')
+
+    def clean_up(self, house: House):
+        """
+        Wife cleans up in the house. The quantity
+        of dirt decreases by 100 points.
+        :param house: the house where wife cleans up
+        """
+        if house.get_dirt() > 100:
+            house.set_dirt(house.get_dirt() - 100)
+        else:
+            house.set_dirt(0)
+        self.set_satiety(self.get_satiety() - 10)
 
 
 class Cat:
-    """The class describing cat."""
+    """
+    The class describing cat. Each action (except eat)
+    decreases degree of satiety by 10 points.
+    """
     def __init__(self, name: str, satiety: int = 30):
         """
         Class constructor.
@@ -176,5 +264,13 @@ class Cat:
 
     def sleep(self):
         """Cat sleeps."""
-        pass
+        self.set_satiety(self.get_satiety() - 10)
 
+    def tear_wallpapers(self, house: House):
+        """
+        Cat tears the wallpapers. It increases quantity of dirt by five points in house
+        where cat lives.
+        :param house: house where cat lives
+        """
+        house.set_dirt(house.get_dirt() + 5)
+        self.set_satiety(self.get_satiety() - 10)
