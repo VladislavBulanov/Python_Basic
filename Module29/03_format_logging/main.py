9e739250a0dffefc4ab64f51d4699f6a4a4f241f
Vladislav Bulanov <solist95@mail.ru>
2023-04-24 17:05:35 +0300

    Done: task n.3

--- Module29/03_format_logging/main.py
+++ Module29/03_format_logging/main.py
@@ -1,13 +1,50 @@
+from typing import Callable
 from functools import wraps
+from datetime import datetime
+from time import time
+
+
+def timer(cls, func: Callable, date_format: str) -> Callable:
+    """
+    Decorator describes start time of the function
+    and time of function's performing.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result_format = date_format
+        for symbol in result_format:
+            if symbol.isalpha():
+                result_format = result_format.replace(symbol, '%' + symbol)
+
+        print('- Запускается "{}.{}". Дата и время запуска: {}'.format(
+                cls.__name__,
+                func.__name__,
+                datetime.now().strftime(result_format),
+        ))
+
+        start_time = time()
+        result = func(*args, **kwargs)
+        end_time = time()
+
+        print('- Завершение "{}.{}", время работы = {} сек.'.format(
+            cls.__name__,
+            func.__name__,
+            round(end_time - start_time, 3),
+        ))
+        return result
+    return wrapper
 
 
 def log_methods(date_format: str):
-    """Class decorator logging all class methods (exclude magic methods)."""
-    @wraps(cls)
-    def wrapper(cls):
-
-    return wrapper
-
+    """Class decorator logging all class' methods (exclude magic methods)."""
+    def decorate(cls):
+        for method in dir(cls):
+            if not method.startswith('__'):
+                current_method = getattr(cls, method)
+                decorated_method = timer(cls, current_method, date_format)
+                setattr(cls, method, decorated_method)
+        return cls
+    return decorate
 
 
 @log_methods("b d Y - H:M:S")
