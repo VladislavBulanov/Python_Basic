fc97c7cc9c12c6090c8b9d05ef8f9e48a018bbfd
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-04-24 15:40:17 +0300

    Backup: 2023-04-24_nb_ubuntu

--- Module29/03_format_logging/main.py
+++ Module29/03_format_logging/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+from functools import wraps
+
+
+def log_methods(date_format: str):
+    """Class decorator logging all class methods (exclude magic methods)."""
+    @wraps(cls)
+    def wrapper(cls):
+
+    return wrapper
+
+
+
+@log_methods("b d Y - H:M:S")
+class A:
+    def test_sum_1(self) -> int:
+        print('test sum 1')
+        number = 100
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+@log_methods("b d Y - H:M:S")
+class B(A):
+    def test_sum_1(self):
+        super().test_sum_1()
+        print("Наследник test sum 1")
+
+    def test_sum_2(self):
+        print("test sum 2")
+        number = 200
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+my_obj = B()
+my_obj.test_sum_1()
+my_obj.test_sum_2()
--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from typing import Callable
+from functools import wraps
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *args, **kwargs):
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
