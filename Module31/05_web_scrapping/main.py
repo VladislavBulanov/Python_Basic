60a3a62ce1f2d7961b42d2f148f72ae26b0639ed
Vladislav Bulanov <vladislav.bulanov1895@gmail.com>
2023-05-02 19:45:04 +0300

    Backup: 2023-05-02_nb_ubuntu

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,6 +1,30 @@
-# TODO здесь писать код
+from re import findall
+from typing import List
+import requests
+import json
 
-# В данном случае запрос request.get заменен на загрзку сайта из файла html
-with open('examples.html', 'r') as f:
-    text = f.read()
-# По итогу вы так же получаете код сайта в виде одной строки
+
+def get_tag_content(tag: str, data: str) -> List[str]:
+    """
+    The function returns list of contents of specified HTML-tag.
+    :param tag: HTML-tag
+    :param data: source HTML-code
+    """
+    pattern = fr'>.*</{tag}>'
+    closing_tags_length = len(tag) + 3
+    matches = findall(pattern, data)
+    result = [match[1:-closing_tags_length] for match in matches]
+    return result
+
+
+if __name__ == '__main__':
+    # Test 1:
+    with open('examples.html', 'r') as f:
+        text = f.read()
+    print(get_tag_content('h3', text))
+
+    # Test 2:
+    # TODO
+    request = requests.get('https://nhl.com/')
+    data = json.loads(request.text)
+    print(data)
--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -1,1 +1,46 @@
-# TODO здесь писать код
+import json
+from typing import Optional, List
+
+
+def get_data_from_json(filename: str) -> Optional[dict]:
+    """
+    The function returns dictionary with data from specified JSON-file.
+    :param filename: name of JSON-file
+    """
+    try:
+        with open(f'{filename}.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print('Указанный файл не найден.')
+
+
+def compare_dictionaries(
+        dictionary_1: dict, dictionary_2: dict, diff_list: List[str]
+) -> dict:
+    """
+    The function compares two dictionaries by specified keys. If values are
+    different the value of second dictionary is added in result dictionary.
+    :param dictionary_1: first dictionary (old data)
+    :param dictionary_2: second dictionary (new data)
+    :param diff_list: keys by which the differences are searching for
+    :return: dictionary with the newest data
+    """
+    result_dictionary = dict()
+    for key, value in dictionary_1.items():
+        if (dictionary_1[key] != dictionary_2[key]) and (key in diff_list):
+            result_dictionary[key] = dictionary_2[key]
+        else:
+            if isinstance(value, dict):
+                result = compare_dictionaries(value, dictionary_2[key], diff_list)
+                if result:
+                    result_dictionary[value] = result
+    return result_dictionary
+
+
+if __name__ == '__main__':
+    old_data = get_data_from_json('json_old')
+    new_data = get_data_from_json('json_new')
+    difference_list = ["services", "staff", "datetime"]
+    changed_data = compare_dictionaries(old_data, new_data, difference_list)
+    print(changed_data if changed_data
+          else 'Данные по указанным ключам не изменились.')
